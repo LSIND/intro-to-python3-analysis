43cbaaf89b125eafdf0e753cbcfbf4581098d186
Daria Chemkaeva <l_s_ind@hotmail.com>
2019-11-28 12:10:35 +0300

    Update main.py

--- CountFilesAndFolders/main.py
+++ CountFilesAndFolders/main.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+'''py -3 main.py C:\Users\Admin\Downloads\intro-to-python3-analysis-master'''
 
 import os
+import sys
 
-for root, dirs, files in os.walk('intro-to-python3-analysis-master'):
-    path = root.split(os.sep)
+def main():
+    userpath = sys.argv[1]
+    for root, dirs, files in os.walk(userpath):
+        path = root.split(os.sep)
 
-    count_dirs = count_files = 0
-    for f in dirs:
-        count_dirs += 1
+        count_dirs = count_files = 0
+        for f in dirs:
+            count_dirs += 1
 
-    for file in files:
-        count_files += 1
-    print((len(path) - 2) * '--'+'>', os.path.basename(root), ':\t', count_dirs, 'folders, ', count_files, 'files')
+        for file in files:
+            count_files += 1
+        print((len(path) - 2) * '--'+'>', os.path.basename(root), ':\t', count_dirs, 'folders, ', count_files, 'files')
+
+main()
